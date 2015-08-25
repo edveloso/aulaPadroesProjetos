6957ea4964b08ff90b377a3a871b156e94927ac7
ednilson veloso <edveloso@gmail.com>
2015-08-25 16:03:56 +0000

    add  from c9

--- src/VendaTest.java
+++ src/VendaTest.java
@@ -1,33 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
-
-import junit.framework.Assert;
 import modelo.Cliente;
-import modelo.Item;
-import modelo.Produto;
-import modelo.Venda;
-
-import org.junit.Test;
-
 
 public class VendaTest {
 
-	@Test
-	public void test() {
-		Venda venda = new Venda();
-		venda.setNumero(1);
-		List<Item> itens = new ArrayList<Item>();
-		Item item = new Item();
-		item.setQuantidade(10);
-		Produto produto = new Produto();
-		produto.setCodigo(123);
-		produto.setDescricao("Manteiga qualy");
-		produto.setValorUnitario(2.99);
-		item.setProduto(produto);
-		itens.add(item);
-		venda.setItens(itens);
-		Assert.assertEquals(venda.getNumero(), 1);
-		Assert.assertEquals(venda.getItens().size(), 1);
+
+	public static void main (String args[]){
+		
+		Cliente cliente = new Cliente();
+		cliente.setNome("Jose");
+		System.out.println(cliente.getNome());
+		
+		
 	}
 
 }
